233d1e967c1642fc3506320b5902d9ebe5d0ccc5
Gabriel de Tassigny <gabriel@multivac.cc>
2018-12-02 11:04:01 +0900

    Add tinymce anchor and links plugins

--- frontend/public/js/admin-post-form.js
+++ frontend/public/js/admin-post-form.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
     tinymce.init({
         selector:'.wysiwyg-input',
-        toolbar: 'image codesample',
-        plugins: 'image imagetools codesample',
+        toolbar: 'image codesample anchor link',
+        plugins: 'image imagetools codesample anchor link',
+        menubar: 'insert',
         images_upload_credentials: true,
         images_upload_handler: function (blobInfo, success, failure) {
             var formData = new FormData();
