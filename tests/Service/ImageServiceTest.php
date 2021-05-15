f08ba095faea7c6680b3c8ca21e0b64713a2a244
Gabriel de Tassigny <gabriel@multivac.cc>
2021-05-15 21:02:36 +0200

    Remove unused use statement

--- tests/Service/ImageServiceTest.php
+++ tests/Service/ImageServiceTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace GabrielDeTassigny\Blog\Tests\Service;
 
-use DateTime;
 use GabrielDeTassigny\Blog\Service\ImageService;
 use GabrielDeTassigny\Blog\Service\Storage\StorageFactory;
 use GabrielDeTassigny\Blog\Service\Storage\StorageInterface;
